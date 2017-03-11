2b1bb653f779488a3674124eedd301b4c8f6052a
kmelve <knut@netliferesearch.com>
2017-03-11 17:00:00 +0100

    wip

--- index.js
+++ index.js
@@ -8,8 +8,59 @@ var dataSeries = lessData.map((line, index) => ([
     line[0]
 ]))
 
-regression('linear', dataSeries)
+/**
+ * Set up the Board
+ */
 
+var five = require('johnny-five')
+var Tessel = require('tessel-io')
+var board = new five.Board({
+  io: new Tessel(),
+  repl: false,
+  debug: true
+})
+/**
+ * Set up Slackbot and fetch the config
+ */
+var bot = require('./bot')
+var botConfig = require('./botconfig')
+var messages = botConfig.messages
+var user = botConfig.user
+var params = botConfig.params
+var thresholdTemp = botConfig.thresholdTemp
+var channel = botConfig.channel
+var group = botConfig.group
+
+function botStartUp () {
+  bot.on('start', function () {
+    console.log(user, messages.ready, params)
+  })
+}
+
+function printToLCD (string, temp) {
+  string = string || 'Klar'
+  /**
+   * Set up the LCD Screen (on port B)
+   * */
+  console.log(string, temp)
+}
+
+function postToSlack (message) {
+  if (user) {
+    console.log(user, message, params)
+  }
+  if (channel) {
+    console.log(channel, message, params)
+  }
+  if (group) {
+    console.log(group, message, params)
+  }
+}
+
+
+// regression('linear', dataSeries)
+
+/*
 dataSeries.forEach((line, i) => {
     if (i < 50) {
         return;
@@ -19,6 +70,44 @@ dataSeries.forEach((line, i) => {
         line,
         regression('linear', dataSeries.slice(i - 10, i)).equation[0] > 0.0075
     ));
+*/
+
+board.on('ready', function () {
+  var multi = new five.Multi({
+    controller: 'BME280'
+  })
+  var led = new five.Led('B0')
+  led.blink(500)
+  /**
+   * Set the initial brewing state and start up
+   */
+  var brewing = false
+  botStartUp()
+
+  /**
+   * Temperature logic
+   */
+  multi.on('data', function () {
+    printToLCD(messages.ready, temp)
+    var temp = this.thermometer.celsius
+    setTimeout(printToLCD(messages.ready, temp), 1000)
+    if (temp > thresholdTemp && !brewing) {
+      brewing = true
+      printToLCD(messages.brewing, temp)
+      postToSlack(messages.brewingSlack)
+    }
+    if (temp > thresholdTemp && brewing) {
+      printToLCD(messages.brewing, temp)
+    }
+    if (temp < thresholdTemp && brewing) {
+      brewing = false
+      printToLCD(messages.done, temp)
+      postToSlack(messages.done)
+    }
+    if (temp < thresholdTemp && !brewing) {
+      setTimeout(printToLCD(messages.ready, temp), 5000)
+    }
+  })
 })
 
 // lessData.forEach((line, i) => {
